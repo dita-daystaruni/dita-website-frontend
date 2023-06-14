aa4969b9b1d483817e75dcc952cb912b089d7056
Richie Mugambi <richiemugambi4@gmail.com>
2023-06-14 09:07:38 +0300

    chore: transferred code from dita-website to dita-website-frontend
    This is to ensure that there are separate development repositories

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Home from './pages/home/home'
+
+const App = () => {
+  return (
+    <div>
+      <Home/>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  mode: 'jit',
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,jsx}"
+  ],
+  darkMode: 'class', 
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
