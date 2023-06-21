17a215caa0396924e2959e52d570906ee5efa7cf
Richie Mugambi <richiemugambi4@gmail.com>
2023-06-21 14:13:23 +0300

    feat: added services section

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import About from "../components/landing/about";
 import EventsLayout from "../components/landing/events/eventsLayout";
+import Services from "../components/landing/services";
 
 const Home = () => {
   return (
     <main>
       <About />
       <EventsLayout/>
+      <Services/>
     </main>
   );
 };
