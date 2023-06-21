a4edb0ea663ed9262018f012560b0c3f05ecdfb3
Richie Mugambi <richiemugambi4@gmail.com>
2023-06-21 12:51:00 +0300

    feat: added flex on layout to center items

--- src/pages/components/landing/events/eventsLayout.jsx
+++ src/pages/components/landing/events/eventsLayout.jsx
@@ -1,28 +1,30 @@
-import React from 'react'
+import React from "react";
 
-import EventsHeadline from './eventsHeadline'
-import EventCards from './eventCards';
+import EventsHeadline from "./eventsHeadline";
+import EventCards from "./eventCards";
 
 const EventsLayout = () => {
   return (
     <main className="bg-black text-white">
       <EventsHeadline />
-      <div className="grid grid-cols-3 max-width items-center mt-4">
-        <EventCards
-          heading="Prodi Informatika Raih Akredit.."
-          content="UKM AMIK menjadi Perwakilan.. "
-        />
-        <EventCards
-          heading="Diseminasi Teknologi CSRG"
-          content="CSRG menjadi pemateri seminar wawasan teknologi ke beberapa pelosok negeri guna mendukung wawasan teknologi merata. "
-        />
-        <EventCards
-          heading="UKM AMIK menjadi Perwakilan.. "
-          content="CSRG menjadi satu-satunya perwakilan dari Indonesia di event Engineering Con- ferences, USA."
-        />
+      <div className="flex justify-center ">
+        <div className="grid grid-cols-3 gap-10 items-center mt-4">
+          <EventCards
+            heading="Prodi Informatika Raih Akredit.."
+            content="UKM AMIK menjadi Perwakilan.. "
+          />
+          <EventCards
+            heading="Diseminasi Teknologi CSRG"
+            content="CSRG menjadi pemateri seminar wawasan teknologi ke beberapa pelosok negeri guna mendukung wawasan teknologi merata. "
+          />
+          <EventCards
+            heading="UKM AMIK menjadi Perwakilan.. "
+            content="CSRG menjadi satu-satunya perwakilan dari Indonesia di event Engineering Con- ferences, USA."
+          />
+        </div>
       </div>
     </main>
   );
-}
+};
 
-export default EventsLayout
+export default EventsLayout;
