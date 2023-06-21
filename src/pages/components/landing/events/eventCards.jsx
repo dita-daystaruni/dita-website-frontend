31ce7fc459d88300d49b8097dd31baa0b57db670
Richie Mugambi <richiemugambi4@gmail.com>
2023-06-21 12:34:38 +0300

    fix: removed href element in tag

--- src/pages/components/landing/events/eventCards.jsx
+++ src/pages/components/landing/events/eventCards.jsx
@@ -4,26 +4,23 @@ import EventsBtn from './eventsBtn';
 const EventCards = ({heading,content}) => {
   return (
     <div className="max-w-sm bg-custom-navy-blue border-gray-200 shadow dark:bg-gray-800 dark:border-gray-700">
-      <a href="#">
+      <div>
         {/* TODO: ADD IMAGES TO RESPECTIVE CARDS */}
-        <img
-          src="/docs/images/blog/image-1.jpg"
-          alt=""
-        />
-      </a>
+        <img src="/docs/images/blog/image-1.jpg" alt="" />
+      </div>
       <div className="p-5">
-        <a href="#">
+        <div>
           <h5 className="mb-2 text-2xl font-bold tracking-tight">
             {/*TODO: DYNAMICALLY CHANGE HEADING*/}
             {heading}
           </h5>
-        </a>
+        </div>
         <p className="mb-3 font-normal">
           {/* TODO: DYNAMICALLY CHANGE CONTENT*/}
           {content}
         </p>
         <EventsBtn />
-        <p className="border-t-0">25 Januari 2023 ・ 10:05</p>
+        <p className="border-t-0">ICT theater ・ 25 Januari 2023 ・ 10:05</p>
       </div>
     </div>
   );
