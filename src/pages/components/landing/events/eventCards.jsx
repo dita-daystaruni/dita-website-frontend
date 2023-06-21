304934457877559fa1e748049b345844b7992410
Richie Mugambi <richiemugambi4@gmail.com>
2023-06-21 12:50:32 +0300

    feat: added top border on events details

--- src/pages/components/landing/events/eventCards.jsx
+++ src/pages/components/landing/events/eventCards.jsx
@@ -3,24 +3,28 @@ import EventsBtn from './eventsBtn';
 
 const EventCards = ({heading,content}) => {
   return (
-    <div className="max-w-sm bg-custom-navy-blue border-gray-200 shadow dark:bg-gray-800 dark:border-gray-700">
-      <div>
-        {/* TODO: ADD IMAGES TO RESPECTIVE CARDS */}
-        <img src="/docs/images/blog/image-1.jpg" alt="" />
-      </div>
-      <div className="p-5">
+    <div className="flex flex-col h-full">
+      <div className="max-w-sm bg-custom-navy-blue border-gray-200 shadow dark:bg-gray-800 dark:border-gray-700">
         <div>
-          <h5 className="mb-2 text-2xl font-bold tracking-tight">
-            {/*TODO: DYNAMICALLY CHANGE HEADING*/}
-            {heading}
-          </h5>
+          {/* TODO: ADD IMAGES TO RESPECTIVE CARDS */}
+          <img src="/docs/images/blog/image-1.jpg" alt="" />
         </div>
-        <p className="mb-3 font-normal">
-          {/* TODO: DYNAMICALLY CHANGE CONTENT*/}
-          {content}
-        </p>
-        <EventsBtn />
-        <p className="border-t-0">ICT theater ・ 25 Januari 2023 ・ 10:05</p>
+        <div className="p-5">
+          <div>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight">
+              {/*TODO: DYNAMICALLY CHANGE HEADING*/}
+              {heading}
+            </h5>
+          </div>
+          <p className="mb-3 font-normal">
+            {/* TODO: DYNAMICALLY CHANGE CONTENT*/}
+            {content}
+          </p>
+          <EventsBtn />
+          <div className="border-t border-black">
+            <p className="mt-4">ICT theater ・ 25 Januari 2023 ・ 10:05</p>
+          </div>
+        </div>
       </div>
     </div>
   );
