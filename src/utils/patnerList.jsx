7bbb0354f071fe17440e516d145c148edce5aec7
Richie Mugambi <richiemugambi4@gmail.com>
2023-06-21 16:25:25 +0300

    feat: added patnerList utility

---
+++ src/utils/patnerList.jsx
@@ -0,0 +1,31 @@
+import googleLogo from '../assets/googleLogo.png';
+import oracleLogo from '../assets/oracleLogo.png';
+import huaweiLogo from '../assets/huaweiLogo.png';
+import ibmLogo from '../assets/ibmLogo.png';
+
+export const patners=[
+    {
+    id:0,
+    name: 'Huawei',
+    logo: huaweiLogo,
+    link: 'https://www.huawei.com/en/'
+    },
+    {
+    id:1,
+    name: 'Google',
+    logo: googleLogo,
+    link: 'https://www.google.com/'
+    },
+    {
+    id:2,
+    name: 'Oracle',
+    logo: oracleLogo,
+    link: 'https://www.oracle.com/'
+    },
+    {
+    id:3,
+    name: 'IBM',
+    logo: ibmLogo,
+    link: 'https://www.ibm.com/'
+    },
+]
