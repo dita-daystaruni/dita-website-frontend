7f25184f61a6c6af4a1f0a0e64cf9cc7475aeb24
Richie Mugambi <richiemugambi4@gmail.com>
2023-06-21 14:33:27 +0300

    feat: added DarkCard and BlueRediredBtn
    This replaced the eventsCard and eventBtn

---
+++ src/pages/components/common/DarkCard.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import BlueRedirectBtn from './blueRedirectBtn';
+
+const DarkCard = ({heading,content}) => {
+  return (
+    <div className="flex flex-col h-full">
+      <div className="max-w-sm bg-custom-navy-blue border-gray-200 shadow dark:bg-gray-800 dark:border-gray-700">
+        <div>
+          {/* TODO: ADD IMAGES TO RESPECTIVE CARDS */}
+          <img src="/docs/images/blog/image-1.jpg" alt="" />
+        </div>
+        <div className="p-5">
+          <div>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight">
+              {/*TODO: DYNAMICALLY CHANGE HEADING*/}
+              {heading}
+            </h5>
+          </div>
+          <p className="mb-3 font-normal">
+            {/* TODO: DYNAMICALLY CHANGE CONTENT*/}
+            {content}
+          </p>
+          <BlueRedirectBtn />
+          <div className="border-t border-black">
+            <p className="mt-4">ICT theater ・ 25 Januari 2023 ・ 10:05</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DarkCard
