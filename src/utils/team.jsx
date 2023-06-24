e33c5aedfe2baad3a7a4524f5991f3436de87f14
Richie Mugambi <richiemugambi4@gmail.com>
2023-06-24 15:51:22 +0300

    feat: added mockup data for the team

---
+++ src/utils/team.jsx
@@ -0,0 +1,23 @@
+import head1 from '../assets/head1.png';
+import head2 from '../assets/head2.png';
+
+export const team=[
+    {
+        id:0,
+        name:"John Doe",
+        role:"CEO",
+        image: head1,
+    },
+    {
+        id:1,
+        name:"Jane Doe",
+        role:"CTO",
+        image: head2,
+    },
+    {
+        id:2,
+        name:"John Smith",
+        role:"CFO",
+        image: head1,
+    },
+]
