e809b22dfd0545ba860c9bc25dff7d2669636822
Richie Mugambi <richiemugambi4@gmail.com>
2023-06-24 15:50:16 +0300

    feat: added teams section

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -4,6 +4,7 @@ import Announcements from "../components/landing/announcements";
 import EventsLayout from "../components/landing/events/eventsLayout";
 import Patnership from "../components/landing/patnership";
 import Services from "../components/landing/services";
+import Team from "../components/landing/team";
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
       <EventsLayout/>
       <Services/>
       <Announcements/>
+      <Team/>
       <Patnership/>
     </main>
   );
