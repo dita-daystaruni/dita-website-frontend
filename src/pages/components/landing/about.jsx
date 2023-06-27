39de010beae2077ea0315ae5b93d60943e19d15c
Richie Mugambi <richiemugambi4@gmail.com>
2023-06-27 13:35:00 +0300

    feat: added responsiveness to about us section

--- src/pages/components/landing/about.jsx
+++ src/pages/components/landing/about.jsx
@@ -7,8 +7,8 @@ import about2 from "../../../assets/unsplash_dsvJgiBJTOs.png";
 const About = () => {
   return (
     <main className="max-w-full bg-gray-200">
-      <section className="flex flex-row justify-between">
-        <div className="py-10 px-10 max-w-lg ml-24 mt-10">
+      <section className="md:flex md:justify-between max-lg:flex-col">
+        <div className="md:py-10 md:px-10 max-w-lg ml-24 p-10 mr-24 max-sm:py-5 max-sm:px-0 max-sm:mx-5">
           <h3 className="font-bold text-2xl mb-5">What is DITA</h3>
           <p>
             Kampus STMIK “AMIKBANDUNG” sebagai perguruan tinggi yang menjadi
@@ -19,20 +19,20 @@ const About = () => {
         <div>
           {/* Picture representing about us picture 1 */}
           <img
-            className="max-w-lg p-10 mr-24"
+            className="md:max-w-lg md:p-10 ml-24 max-sm:p-0 max-sm:ml-0"
             alt="campus view"
             src={about1}
           />
         </div>
       </section>
-      <section className="flex flex-row justify-between">
+      <section className="md:flex md:justify-between max-lg:flex-col">
         {/* Picture representing about us picture 1 */}
         <img
-          className="max-w-lg p-10 ml-24"
+          className="sm:max-w-lg md:p-10 md:ml-24 max-sm:p-0 max-sm:ml:0 max-sm:mt-10"
           alt="lecture theater"
           src={about2}
         />
-        <div className="py-10 px-10 max-w-lg ml-24 mt-10 p-10 mr-24">
+        <div className="md:py-10 md:px-10 md:max-w-lg md:ml-24 md:mt-10 md:p-10 md:mr-24 max-sm:px-0 py-8">
           <h3 className="font-bold text-2xl mb-5 text-right">
             SEE OUR PROJECTS
           </h3>
