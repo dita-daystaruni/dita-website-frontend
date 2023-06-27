828b3b1a790d1e5399f836525c552f4458fb39c8
Richie Mugambi <richiemugambi4@gmail.com>
2023-06-27 15:13:36 +0300

    feat: added responsiveness to announcements

--- src/pages/components/landing/announcements.jsx
+++ src/pages/components/landing/announcements.jsx
@@ -5,9 +5,9 @@ import GreyRedirectBtn from "../common/greyRedirectBtn";
 
 const Announcements = () => {
   return (
-    <section className="flex flex-row justify-between bg-black text-white">
+    <section className="flex justify-between bg-black text-white max-sm:flex-col">
       {/* Div for first flex item */}
-      <div className="py-10 px-10 max-w-xl ml-24 mt-10 p-10 mr-24">
+      <div className="py-10 ml-24 mt-10 mr-24 max-md:px-0 max-sm:mx-0 max-sm:py-5 max-sm:px-5">
         <h3 className="font-bold text-2xl mb-10">ANNOUNCEMENTS</h3>
         <h3 className="font-bold mb-3 text-lg">
           Beasiswa Full Bagi Mahasiswa Berminat *S & K Berlaku
@@ -27,7 +27,7 @@ const Announcements = () => {
         <GreyRedirectBtn margin_right={0} margin_top_bottom="5" />
       </div>
       {/* Div for second flex item */}
-      <div className="py-10 px-10 max-w-xl mr-24 mt-10 p-10 ml-24">
+      <div className="py-10 max-w-xl mr-24 mt-10 ml-24 max-sm:mx-0 max-sm:px-5">
         {/* Picture representing events venue */}
         <img src={announcements} alt="events venue" />
         <p>
