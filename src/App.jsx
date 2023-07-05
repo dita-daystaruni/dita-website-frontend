c4743a1bab3395c1b42786d263fd000ab505e2ea
Richie Mugambi <richiemugambi4@gmail.com>
2023-07-05 14:13:01 +0300

    feat: added provider for react-icons to change sizing of icons globally

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Home from './pages/home/home'
+import { IconContext } from "react-icons";
+
 
 const App = () => {
   return (
     <div>
+      <IconContext.Provider value={{ size: "20"}}>
       <Home/>
+      </IconContext.Provider>
     </div>
   )
 }
