42805639338b81b7d0d01817b78b6f2bbc12fb30
Manu Lamo <straussmoabit@gmail.com>
2023-07-11 21:50:13 +0300

    fixing CI checks

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ export default {
   darkMode: 'class', 
   theme: {
     extend: {
+      backgroundImage: {
+        'header-photo': "url('./src/assets/unsplash_dsvJgiBJTOs.png')",
+      },
       colors: {
         'custom-navy-blue': '#1E1E1E',
       }
