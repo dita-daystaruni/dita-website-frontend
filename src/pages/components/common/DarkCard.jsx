056a751eeb54f1c82f8fffca0045cd85a75186ec
Richie Mugambi <richiemugambi4@gmail.com>
2023-07-11 14:58:09 +0300

    feat: added animations default options

--- src/pages/components/common/DarkCard.jsx
+++ src/pages/components/common/DarkCard.jsx
@@ -1,34 +1,38 @@
-import React from 'react'
-import BlueRedirectBtn from './blueRedirectBtn';
+import React from "react";
+import BlueRedirectBtn from "./blueRedirectBtn";
 import dummyImage from "../../../assets/unsplash_dsvJgiBJTOs.png";
+import { defaultOptions } from "../../../utils/animations";
+import { Tilt } from "react-tilt";
 
-const DarkCard = ({heading,content}) => {
+const DarkCard = ({ heading, content }) => {
   return (
-    <div className="flex flex-col h-full">
-      <div className="max-w-sm bg-custom-navy-blue border-gray-200 shadow dark:bg-gray-800 dark:border-gray-700">
-        <div>
-          {/* TODO: ADD IMAGES TO RESPECTIVE CARDS */}
-          <img src={dummyImage} alt="" />
-        </div>
-        <div className="p-5">
+    <Tilt options={defaultOptions} className="cursor-pointer">
+      <div className="flex flex-col h-full">
+        <div className="max-w-sm bg-custom-navy-blue border-gray-200 shadow dark:bg-gray-800 dark:border-gray-700">
           <div>
-            <h5 className="mb-2 text-2xl font-bold tracking-tight">
-              {/*TODO: DYNAMICALLY CHANGE HEADING*/}
-              {heading}
-            </h5>
+            {/* TODO: ADD IMAGES TO RESPECTIVE CARDS */}
+            <img src={dummyImage} alt="" />
           </div>
-          <p className="mb-3 font-normal">
-            {/* TODO: DYNAMICALLY CHANGE CONTENT*/}
-            {content}
-          </p>
-          <BlueRedirectBtn />
-          <div className="border-t border-black">
-            <p className="mt-4">ICT theater ・ 25 Januari 2023 ・ 10:05</p>
+          <div className="p-5">
+            <div>
+              <h5 className="mb-2 text-2xl font-bold tracking-tight">
+                {/*TODO: DYNAMICALLY CHANGE HEADING*/}
+                {heading}
+              </h5>
+            </div>
+            <p className="mb-3 font-normal">
+              {/* TODO: DYNAMICALLY CHANGE CONTENT*/}
+              {content}
+            </p>
+            <BlueRedirectBtn />
+            <div className="border-t border-black">
+              <p className="mt-4">ICT theater ・ 25 Januari 2023 ・ 10:05</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Tilt>
   );
-}
+};
 
-export default DarkCard
+export default DarkCard;
---
+++ src/utils/animations.jsx
@@ -0,0 +1,11 @@
+export const defaultOptions = {
+  reverse: false, // reverse the tilt direction
+  max: 35, // max tilt rotation (degrees)
+  perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
+  scale: 1.1, // 2 = 200%, 1.5 = 150%, etc..
+  speed: 700, // Speed of the enter/exit transition
+  transition: true, // Set a transition on enter/exit.
+  axis: null, // What axis should be disabled. Can be X or Y.
+  reset: true, // If the tilt effect has to be reset on exit.
+  easing: "cubic-bezier(.03,.98,.52,.99)", // Easing on enter/exit.
+};
