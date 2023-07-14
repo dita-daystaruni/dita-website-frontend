dd9d320161d241dfd0aa1486aa1efc544563f283
Manu Lamo <straussmoabit@gmail.com>
2023-07-14 12:07:21 +0300

    refacor:removal of sidebar

--- src/pages/components/landing/header.jsx
+++ src/pages/components/landing/header.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Navbar from '../common/navbar'
-import Sidebar from '../common/sidebar'
 import Chatbot from '../common/chatbot'
 
 const Header = () => {
@@ -8,7 +7,7 @@ const Header = () => {
     <main className="flex flex-col h-[90vh] w-screen
                      bg-purple-600">
     <Navbar/>
-    <Sidebar/>
+   
     <div className="h-3/5 p-5 max-md:p-6 mx-auto my-auto w-screen flex flex-col justify-between">
         <h1 className='text-7xl font-bold font-sans ml-20 my-auto dark:text-white max-sm:text-5xl max-sm:text-center max-sm:ml-0'>DITA WEBSITE</h1>
     </div>
