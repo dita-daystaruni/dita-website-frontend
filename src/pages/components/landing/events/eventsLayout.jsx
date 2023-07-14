7607df8c07b1c4237b38353d8c7ffc637037c09d
Richie Mugambi <richiemugambi4@gmail.com>
2023-07-14 15:54:01 +0300

    feat: removed template

--- src/pages/components/landing/events/eventsLayout.jsx
+++ src/pages/components/landing/events/eventsLayout.jsx
@@ -1,26 +1,44 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import EventsHeadline from "./eventsHeadline";
 import DarkCard from "../../common/DarkCard";
 
+
 const EventsLayout = () => {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const response = await axios.get(
+          "https://dita-website.vercel.app/api/v1/events/all"
+        );
+        setEvents(response.data.event); // Access nested 'event' property
+      } catch (error) {
+        console.error("Error fetching events:", error);
+      }
+    };
+
+    fetchEvents();
+  }, []);
+
   return (
     <main className="bg-black text-white pb-10">
       <EventsHeadline />
       <div className="flex justify-center md:px-5 ">
         <div className="grid grid-cols-3 gap-10 items-center mt-4 max-sm:grid-cols-1">
-          <DarkCard
-            heading="Prodi Informatika Raih Akredit.."
-            content="UKM AMIK menjadi Perwakilan.. "
-          />
-          <DarkCard
-            heading="Diseminasi Teknologi CSRG"
-            content="CSRG menjadi pemateri seminar wawasan teknologi ke beberapa pelosok negeri guna mendukung wawasan teknologi merata. "
-          />
-          <DarkCard
-            heading="UKM AMIK menjadi Perwakilan.. "
-            content="CSRG menjadi satu-satunya perwakilan dari Indonesia di event Engineering Con- ferences, USA."
-          />
+          {events.map((event) => (
+            <section key={event._id}>
+              <DarkCard
+                title={event.title}
+                description={event.description}
+                image={event.image}
+                date={event.date}
+                location={event.location}
+                time={event.time}
+              />
+            </section>
+          ))}
         </div>
       </div>
     </main>
