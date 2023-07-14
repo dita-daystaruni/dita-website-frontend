463a58592c91a9f88b93a4c2905d175b28a885d1
Richie Mugambi <richiemugambi4@gmail.com>
2023-07-14 14:20:48 +0300

    feat: added props for button incase text is changed

--- src/pages/components/common/greyRedirectBtn.jsx
+++ src/pages/components/common/greyRedirectBtn.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { BsFillArrowRightCircleFill } from "react-icons/bs";
 
 
-const GreyRedirectBtn = ({margin_right,margin_top_bottom}) => {
+const GreyRedirectBtn = ({margin_right,margin_top_bottom,text}) => {
   return (
-    <button className={`flex gap-4 ${margin_right ? margin_right :""} ${margin_top_bottom ? margin_top_bottom : ''}`}>
+    <button className={`flex hover:underline gap-4 ${margin_right ? margin_right :""} ${margin_top_bottom ? margin_top_bottom : ''}`}>
       <BsFillArrowRightCircleFill size={30} color="#656f77" />
-      <p className="mt-1">Read more about this news</p>
+      <p className="mt-1">{text?text:"Read more about this"}</p>
     </button>
   );
 }
