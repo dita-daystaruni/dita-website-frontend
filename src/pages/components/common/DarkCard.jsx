eb823a382f383df2524b102b899502dcbb5bc136
Richie Mugambi <richiemugambi4@gmail.com>
2023-07-14 15:53:17 +0300

    feat: added props for events

--- src/pages/components/common/DarkCard.jsx
+++ src/pages/components/common/DarkCard.jsx
@@ -1,32 +1,31 @@
 import React from "react";
 import BlueRedirectBtn from "./blueRedirectBtn";
-import dummyImage from "../../../assets/unsplash_dsvJgiBJTOs.png";
 import { defaultOptions } from "../../../utils/animations";
 import { Tilt } from "react-tilt";
 
-const DarkCard = ({ heading, content }) => {
+const DarkCard = ({ title, date,image,description,location,time }) => {
   return (
     <Tilt options={defaultOptions} className="cursor-pointer">
       <div className="flex flex-col h-full">
         <div className="max-w-sm bg-custom-navy-blue border-gray-200 shadow dark:bg-gray-800 dark:border-gray-700">
           <div>
             {/* TODO: ADD IMAGES TO RESPECTIVE CARDS */}
-            <img src={dummyImage} alt="" />
+            <img src={image} alt="" />
           </div>
           <div className="p-5">
             <div>
               <h5 className="mb-2 text-2xl font-bold tracking-tight">
                 {/*TODO: DYNAMICALLY CHANGE HEADING*/}
-                {heading}
+                {title}
               </h5>
             </div>
             <p className="mb-3 font-normal">
               {/* TODO: DYNAMICALLY CHANGE CONTENT*/}
-              {content}
+              {description}
             </p>
-            <BlueRedirectBtn />
+            {/* <BlueRedirectBtn /> */}
             <div className="border-t border-black">
-              <p className="mt-4">ICT theater ・ 25 Januari 2023 ・ 10:05</p>
+              <p className="mt-4">{location} ・ {date} ・ {time}</p>
             </div>
           </div>
         </div>
